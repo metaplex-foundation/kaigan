362e91c1ad595611396d940d6236599d34457f4d
febo <febo@metaplex.com>
2023-08-22 18:11:27 +0300

    Add derive traits

--- src/types.rs
+++ src/types.rs
@@ -10,6 +10,7 @@ use borsh::{BorshDeserialize, BorshSerialize};
 /// This is useful for deserializing a vector that does not have
 /// a length prefix. In order to determine how many elements to deserialize,
 /// the type of the elements must implement the trait `Sized`.
+#[derive(Clone, Eq, PartialEq)]
 pub struct RemainderVec<T: BorshSerialize + BorshDeserialize>(Vec<T>);
 
 /// Deferences the inner `Vec` type.
